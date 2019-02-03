6beef5aaad556fbcf64ff8cbf85beedcc2836563
Raphael Ugha <raphaelugha@gmail.com>
2019-02-03 11:47:23 +0000

    add route for getting document by id

--- server/db/eventsProvider.js
+++ server/db/eventsProvider.js
@@ -1,28 +1,40 @@
-const EventsProvider = function(elasticClient) {
+const EventsProvider = function (elasticClient) {
   this.elasticClient = elasticClient;
   this.mapping = {
-    index: "blog",
+    index: "events",
     type: "events",
     body: {
       properties: {
-        title: {type: "text"},
-        content: {type: "text"},
+        title: {
+          type: "text"
+        },
+        content: {
+          type: "text"
+        },
         //date: {type: "date"},
       }
     }
   }
 }
 
-EventsProvider.prototype.addDocument = function (document) {
+EventsProvider.prototype.addEvent = function (event) {
   return this.elasticClient.index({
     index: this.mapping.index,
     type: this.mapping.type,
     body: {
-      title: document.title,
-      content: document.content,
-      //date: document.date,
+      title: event.title,
+      content: event.content,
+      //date: event.date,
     }
   });
 }
 
+EventsProvider.prototype.getEvent = function (id) {
+  const response = this.elasticClient.get({
+    index: 'events',
+    type: 'events',
+    id: id
+  });
+  return response;
+}
 module.exports = EventsProvider;
--- server/routes/events.js
+++ server/routes/events.js
@@ -2,9 +2,30 @@ const express = require('express');
 const router = express.Router();
 const elastic = require('../db/elastic');
 
-const eventsRouter = function(provider) {
-  router.post('/', function (req, res, next) {
-    provider.addDocument(req.body).then(function (result) { res.json(result) });
+const eventsRouter = function (provider) {
+  router.post('/', function (req, res) {
+    provider.addEvent(req.body).then(function (result) {
+      res.json(result)
+    }).catch((err) => {
+      res.status(500);
+      res.json({
+        status: 500,
+        error: err
+      });
+    });
+  });
+
+  //Get document by id
+  router.get('/:id', function (req, res) {
+    provider.getEvent(req.params.id).then(function (result) {
+      res.json(result)
+    }).catch((err) => {
+      res.status(500);
+      res.json({
+        status: 500,
+        error: err
+      });
+    });
   });
 
   return router
