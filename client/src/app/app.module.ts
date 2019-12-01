5f25c927223d75d505c447bb1d3899d5395cb826
Joanna <joanna.gora@protonmail.com>
2019-12-01 18:02:30 +0000

    add home page background

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+
 ];
 
 @NgModule({
