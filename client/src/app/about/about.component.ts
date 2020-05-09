fb36e27c70c2e88d5726bca36a072a40f9de3a75
Joanna <joanna.gora@protonmail.com>
2020-05-09 15:46:55 +0100

    about is not full screen

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss', '../../styles/global.scss']
 })
 export class AboutComponent implements OnInit {
   title = "ABOUT";
